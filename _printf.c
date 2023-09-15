687005fa5b9918391694fc8b0c45a06a36611e81
yssrexe <elyasiniyassir40@gmail.com>
2023-09-15 23:35:08 +0100

    printf

--- _printf.c
+++ _printf.c
@@ -1,43 +1,43 @@
 #include "main.h"
 /*printf*/int _printf(const char *format, ...)/*f*/
 {
-va_list args;
-char *string;
-int n_printed = 0;
-va_start(args, format);
-while (*format != '\0')
-{
-	if (*format != '%')
+	va_list args;
+	char *string;
+	int n_printed = 0;
+	va_start(args, format);
+	while (*format != '\0')
 	{
-	_putchar(*format);
-	n_printed++;
-	}
-	else
-	{
-		format++;
-		if (*format == 'c')
+		if (*format != '%')
 		{
-		_putchar(va_arg(args, int));
-		n_printed++;
-		}
-		else if (*format == 's')
-		{
-		string = va_arg(args, char *);
-			while (*string != '\0')
-			{
-			_putchar(*string);
-			string++;
+			_putchar(*format);
 			n_printed++;
+		}
+		else
+		{
+			format++;
+			if (*format == 'c')
+			{
+				_putchar(va_arg(args, int));
+				n_printed++;
+			}
+			else if (*format == 's')
+			{
+				string = va_arg(args, char *);
+				while (*string != '\0')
+				{
+					_putchar(*string);
+					string++;
+					n_printed++;
+				}
+			}
+			else if (*format  == '%')
+			{
+				_putchar('%');
+				n_printed++;
 			}
 		}
-		else if (*format  == '%')
-		{
-		_putchar('%');
-		n_printed++;
-		}
+		format++;
 	}
-format++;
-}
-va_end(args);
-return (n_printed);
+	va_end(args);
+	return (n_printed);
 }
