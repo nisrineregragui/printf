d4494f8ca79aa6a984b23d56e475845289861bf2
nisrineregragui <nisrineregragui10@gmail.com>
2023-09-15 14:57:29 -0700

    testing the function

--- test.c
+++ test.c
@@ -1,7 +1,13 @@
 #include "main.h"
 int main(void)
 {
+int c;
+char *str = "string";
+char k = 'a';
 _printf("hey\n");
 c = _printf("hey");
+printf("\n%d\n",c);
+_printf("this is a %s\n",str);
+_printf("this is a character %c\n",k);
 return (0);
 }
