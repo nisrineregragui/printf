48aba9d3b95617f1d3d6d859a511133480c23834
nisrineregragui <nisrineregragui10@gmail.com>
2023-09-15 15:19:37 -0700

    tabs

--- _printf.c
+++ _printf.c
@@ -7,35 +7,35 @@ int n_printed = 0;
 va_start(args, format);
 while (*format != '\0')
 {
-if (*format != '%')
-{
-_putchar(*format);
-n_printed++;
-}
-else
-{
-format++;
-if (*format == 'c')
-{
-_putchar(va_arg(args, int));
-n_printed++;
-}
-else if (*format == 's')
-{
-string = va_arg(args, char *);
-while (*string != '\0')
-{
-_putchar(*string);
-string++;
-n_printed++;
-}
-}
-else if (*format  == '%')
-{
-_putchar('%');
-n_printed++;
-}
-}
+	if (*format != '%')
+	{
+	_putchar(*format);
+	n_printed++;
+	}
+	else
+	{
+		format++;
+		if (*format == 'c')
+		{
+		_putchar(va_arg(args, int));
+		n_printed++;
+		}
+		else if (*format == 's')
+		{
+		string = va_arg(args, char *);
+			while (*string != '\0')
+			{
+			_putchar(*string);
+			string++;
+			n_printed++;
+			}
+		}
+		else if (*format  == '%')
+		{
+		_putchar('%');
+		n_printed++;
+		}
+	}
 format++;
 }
 va_end(args);
