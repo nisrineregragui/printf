f795879a5f2339ad4f83313924f08c2ba74ecdf6
nisrineregragui <nisrineregragui10@gmail.com>
2023-09-16 12:50:17 -0700

    m

--- print_binary.c
+++ print_binary.c
@@ -3,9 +3,8 @@
 /*s*/int bin(unsigned int num)/*s*/
 {
 unsigned int binary[32];
-int i = 0;
-int len = 0;
-while(num > 2)
+int i = 0, len = 0;
+        while(num > 2)
                 {
                         binary[i] = num % 2;
                         num /= 2;
