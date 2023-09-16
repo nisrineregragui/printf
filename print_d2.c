1c8a1312428b3d80103e9f961ff5ca967df7199e
nisrineregragui <nisrineregragui10@gmail.com>
2023-09-16 08:14:40 -0700

    teste v2

--- print_d2.c
+++ print_d2.c
@@ -8,33 +8,33 @@
 
 int print_d(int num)
 {
-        int len = 0, i;
-        int digits[10];
-	int a = 0, nega = 0;
-	if(num < 0)
-	{
-	a = 1;
-	num *= -1;
-	nega = 1;}
-        while (num > 0)
-        {
-                digits[len] = num % 10;
-                num /= 10;
-                len++;
-        }
+    int len = 0, i;
+    int digits[10];
+    int a = 0;
 
-        for (i = len - 1; i >= 0; i--)
+    if (num < 0)
+    {
+        a = 1;
+        num = -num;
+        len++; // Increment len for the minus sign
+    }
+
+    while (num > 0)
+    {
+        digits[len] = num % 10;
+        num /= 10;
+        len++;
+    }
+
+    for (i = len - 1; i >= 0; i--)
+    {
+        if (a == 1)
         {
-	if (a == 1)
-	{
-	_putchar('-');
-	a = 0;
-	}
-                _putchar(digits[i] + '0');
+            _putchar('-');
+            a = 0;
         }
-	if (nega == 1)
-	{
-	len++;
-	}
-	return (len);
+        _putchar(digits[i] + '0');
+    }
+
+    return len;
 }
