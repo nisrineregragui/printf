05c7e5e86017e34d3a3418a56245148152a9e156
nisrineregragui <nisrineregragui10@gmail.com>
2023-09-16 08:41:15 -0700

    error

---
+++ print_d.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_d - prints a number
+ * @num: the number to print
+ * Return: the length of the number
+ */
+
+int print_d(int num)
+{
+        int len = 0, i;
+        int digits[10];
+	int a = 0, nega = 0;
+	if (num < 0)
+	{
+		a++;
+	num *= -1;
+	nega++;
+	}
+
+        while (num > 0)
+        {
+                digits[len] = num % 10;
+                num /= 10;
+                len++;
+        }
+
+        for (i = len - 1; i >= 0; i--)
+        {
+		if (a == 1)
+		{
+			_putchar('-');
+			a = 0;
+		}
+
+                _putchar(digits[i] + '0');
+        }
+	if (nega == 1)
+	{
+		len++;
+	}
+        return (len);
+}
