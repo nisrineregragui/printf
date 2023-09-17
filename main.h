94f3f19bb1b30bee81dc87538b2b31ae66eb4a12
yssrexe <elyasiniyassir40@gmail.com>
2023-09-17 01:35:12 +0100

    printf

--- main.h
+++ main.h
@@ -10,6 +10,7 @@ int _putchar(char c);
 int print_d(int num);
 int print_s(char *str);
 int print_handler(const char *format, va_list args);
+int _print_hex(unsigned long int num, char heex);
 
 int print_hex(unsigned int n, unsigned int c);
 int print_x(va_list x);
