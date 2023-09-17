84b3d35e87aaff2098423326a2a5c429717a8b92
yssrexe <elyasiniyassir40@gmail.com>
2023-09-17 01:52:27 +0100

    printf

--- _printf.c
+++ _printf.c
@@ -58,7 +58,7 @@ int print_handler(const char *format, va_list args)
 					len += print_d(va_arg(args, int));
 					break;
 				case 'b':
-					len += _print_hex(va_arg(args, unsigned int), format);
+					len += _print_hex(va_arg(args, unsigned int), format[i]);
 					break;
 				default:
 					len += _putchar('%');
